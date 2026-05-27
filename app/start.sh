afe015cf2ad8de6813d52f16694c15df71a464ad
mau <www.denkozin@gmail.com>
2026-05-28 01:07:23 +0300

    fix: db connection string and start.sh script paths in docker

--- app/start.sh
+++ app/start.sh
@@ -2,10 +2,10 @@
 
 uv run alembic upgrade head
 
-uv run create-admin
+uv run python -m app.scripts.create_admin
 
 if [ "$ENV" = "dev" ]; then
-    uv run seed
+    uv run python -m app.scripts.seed_db
 fi
 
 uv run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
